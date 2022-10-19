20f09b5a48c7a0d03d115278e3adb65a4336a4e5
Oleh Abanin <110968635+olehoutvio@users.noreply.github.com>
2022-10-19 09:50:11 +0300

    Create examples.tsx

---
+++ examples.tsx
@@ -0,0 +1,15 @@
+const { values, setFieldValue } = useFormikContext<{
+    input: string;
+    filters: { [index: string]: string[] };
+  }>();
+ 
+  
+  const onChange = useCallback(
+ (e: ChangeEvent<Record<string, unknown>>, checked: boolean) => {
+   setFieldValue(e.target.name as string, checked);
+ },
+ [setFieldValue],
+);
+
+type Ref<T> = RefCallback<T> | RefObject<T> | null
+
