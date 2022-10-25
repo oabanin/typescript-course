4a65ddb9bbdb7407d8740844f4c21be336327570
Oleh Abanin <110968635+olehoutvio@users.noreply.github.com>
2022-10-25 14:46:37 +0300

    Update Examples-form.jsx

--- Examples-form.jsx
+++ Examples-form.jsx
@@ -26,3 +26,14 @@ const FormComponent = () => {
 
   return <form onSubmit={handleFormEvent}></form>;
 };
+
+
+export declare interface AppProps {
+  children?: React.ReactNode; // best, accepts everything React can render
+  childrenElement: JSX.Element; // A single React element
+  style?: React.CSSProperties; // to pass through style props
+  onChange?: React.FormEventHandler<HTMLInputElement>; // form events! the generic parameter is the type of event.target
+  //  more info: https://react-typescript-cheatsheet.netlify.app/docs/advanced/patterns_by_usecase/#wrappingmirroring
+  props: Props & React.ComponentPropsWithoutRef<"button">; // to impersonate all the props of a button element and explicitly not forwarding its ref
+  props2: Props & React.ComponentPropsWithRef<MyButtonWithForwardRef>; // to impersonate all the props of MyButtonForwardedRef and explicitly forwarding its ref
+}
