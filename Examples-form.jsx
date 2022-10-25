f719f905761888e5e3523e17e165e989150df3e5
Oleh Abanin <110968635+olehoutvio@users.noreply.github.com>
2022-10-25 14:22:06 +0300

    Create Examples-form.jsx

---
+++ Examples-form.jsx
@@ -0,0 +1,11 @@
+import React, { MouseEvent, ReactNode } from 'react'
+type Props = { 
+ onClick(e: MouseEvent<HTMLElement>): void
+ children?: ReactNode 
+}
+
+
+const Button = ({ onClick: handleClick, children }: Props) => (
+  <button onClick={handleClick}>{children}</button>
+)
+
